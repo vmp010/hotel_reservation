288821b39ed3136f764888d2339a72b467496777
QAQBlaze0827 <QAQBlaze5487@gmail.com>
2025-10-22 14:36:05 +0800

    feat:add frontend

---
+++ frontend/hotel/src/main.js
@@ -0,0 +1,6 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import { createApp } from 'vue'
+import App from './App.vue'
+
+createApp(App).mount('#app')
