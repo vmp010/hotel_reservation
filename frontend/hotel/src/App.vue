24a43241afbf59dc51f45b45e473feb5d0661d58
QAQBlaze0827 <QAQBlaze5487@gmail.com>
2025-10-22 15:07:33 +0800

    feat:change css

--- frontend/hotel/src/App.vue
+++ frontend/hotel/src/App.vue
@@ -1,6 +1,6 @@
 <template>
-
+  <Header></Header>
 </template>
 <script setup>
-
+import Header from './components/header.vue'
 </script>
--- frontend/hotel/src/components/header.vue
+++ frontend/hotel/src/components/header.vue
@@ -1,5 +1,5 @@
 <template>
-    
-
-
+  <div class="container-ms bg-primary min-100">
+    三小
+  </div>
 </template>
