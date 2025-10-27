8e4f7d166233bba7b4adaa44fc68714e4d13ddf7
vmp010 <hsunhsun93@gmail.com>
2025-10-27 16:11:10 +0800

    debug:fail

--- backend/main.py
+++ backend/main.py
@@ -48,19 +48,19 @@ def read_root():
 
 #驗光好菜          
 
-#管理每次 API 請求的資料庫連線
-def get_session(): 
-    with Session(engine) as session:
-        yield session
+# #管理每次 API 請求的資料庫連線
+# def get_session(): 
+#     with Session(engine) as session:
+#         yield session
 
-@app.post("/rooms/") #新增，提交
-def create_Room(room:Room,session:Session=Depends(get_session)):
-    session.add(room)
-    session.commit() #提交
-    session.refresh(room)
-    return room #回傳json檔
+# @app.post("/rooms/") #新增，提交
+# def create_Room(room:Room,session:Session=Depends(get_session)):
+#     session.add(room)
+#     session.commit() #提交
+#     session.refresh(room)
+#     return room #回傳json檔
 
-@app.get("/rooms/") #查詢
-def read_rooms(session:Session=Depends(get_session)):
-    rooms=session.query(Room).all()
-    return rooms
+# @app.get("/rooms/") #查詢
+# def read_rooms(session:Session=Depends(get_session)):
+#     rooms=session.query(Room).all()
+#     return rooms
