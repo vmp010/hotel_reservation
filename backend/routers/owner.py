f7ef8f0c3f1e148d6c5296119853ed14e8ee81ef
vmp010 <hsunhsun93@gmail.com>
2025-12-25 22:48:11 +0800

    debug:owner.py

--- backend/routers/owner.py
+++ backend/routers/owner.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 from models import User,Owner
-from auth import get_current_user,db_dependency,bcrypt_context
+from auth import get_current_owner,db_dependency,bcrypt_context
 from schemas import UpdateUserEmailRequest,UpdateUserPasswordRequest
 
 router = APIRouter(
@@ -13,7 +13,7 @@ router = APIRouter(
 async def update_owner_email(
     email_request: UpdateUserEmailRequest,
     db: db_dependency,
-    owner: Owner = Depends(get_current_user)
+    owner: Owner = Depends(get_current_owner)
 ):
     if db.query(Owner).filter(User.email == email_request.new_email).first():
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Email already in use")
@@ -30,7 +30,7 @@ async def update_owner_email(
 async def update_owner_password(
     request: UpdateUserPasswordRequest,
     db: db_dependency,
-    owner: Owner = Depends(get_current_user)
+    owner: Owner = Depends(get_current_owner)
 ):
     if not bcrypt_context.verify(request.old_password, owner.password):
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Current password is incorrect")
